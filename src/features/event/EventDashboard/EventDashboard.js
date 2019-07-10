13021a30bed9439428b802b724225985db36a42f
Dmitry Goronkov <gordima@yahoo.com>
2019-07-10 11:23:07 -0400

    event submit, view and edit created

--- src/features/event/EventDashboard/EventDashboard.js
+++ src/features/event/EventDashboard/EventDashboard.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react'
 import { Grid, Button } from 'semantic-ui-react'
 import EventList from '../EventList/EventList';
 import EventForm from '../EventForm/EventForm';
+import cuid from 'cuid';
 const events = [
   {
     id: '1',
     title: 'Trip to Tower of London',
-    date: '2018-03-27T11:00:00+00:00',
+    date: '2018-03-27',
     category: 'culture',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
@@ -30,7 +31,7 @@ const events = [
   {
     id: '2',
     title: 'Trip to Punch and Judy Pub',
-    date: '2018-03-28T14:00:00+00:00',
+    date: '2018-03-28',
     category: 'drinks',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
@@ -56,22 +57,72 @@ const events = [
 export default class EventDashboard extends Component {
     state = {
         events: events,
-        isOpen: false
+        isOpen: false,
+        selectedEvent: null
     }
-    handleFormToggle = () =>{ 
-        this.setState({isOpen: !this.state.isOpen})
+    // handleFormToggle = () =>{ 
+    //     this.setState({isOpen: !this.state.isOpen})
+    // }
+    handleCreateFormOpen = () => {
+        this.setState({
+            isOpen: true,
+            selectedEvent: null
+        })
     }
+    handleFormCancel = () => {
+        this.setState({
+            isOpen: false
+        })
+    }
+    handleCreateEvent = (newEvent) => {
+        newEvent.id = cuid();
+        newEvent.hostPhotoURL = '/assets/user.png'
+        this.setState(({events})=>({
+            events: [...events, newEvent], isOpen:false
+        }))
+   }
+   handleSelectEvent = (event) => {
+       this.setState({
+           selectedEvent: event,
+           isOpen: true
+       })
+   }
+   handleUpdateEvent = (updatedEvent) => {
+     this.setState(({events}) => ({
+       events: events.map(event => {
+          if (event.id === updatedEvent.id){
+            return {...updatedEvent}
+          } else {
+            return event
+          }
+       }),
+       isOpen: false,
+       selectedEvent: null
+     }))
+   }
+   handleDeleteEvent = (id) => {
+     this.setState(({events}) => ({
+       events: events.filter(e => e.id !== id)
+     }))
+   }
     render() {
-        const {events, isOpen} = this.state;
+        const {events, isOpen, selectedEvent } = this.state;
         return (
             <div>
                  <Grid>
                      <Grid.Column width={10}>
-                        <EventList events={events}></EventList>
+                        <EventList deleteEvent = {this.handleDeleteEvent} selectEvent={this.handleSelectEvent}  events={events}></EventList>
                      </Grid.Column>
                      <Grid.Column width={6}>
-                        <Button onClick={this.handleFormToggle} positive content="Create event"></Button>
-                        {isOpen && <EventForm cancelFormOpen={this.handleFormToggle}></EventForm> } 
+                        <Button onClick={this.handleCreateFormOpen} positive content="Create event"></Button>
+                        {isOpen && 
+                            <EventForm
+                                key={selectedEvent ? selectedEvent.id : 0} 
+                                updateEvent={this.handleUpdateEvent}
+                                selectedEvent={selectedEvent}
+                                createEvent={this.handleCreateEvent}
+                                cancelFormOpen={this.handleFormCancel}>
+                            </EventForm> } 
                      </Grid.Column>
                  </Grid>
             </div>
